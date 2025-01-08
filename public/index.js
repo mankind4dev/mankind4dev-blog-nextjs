959890ecfa4ccc3ab3f21fbb77aa9448adc85916
mankind4dev <ogunmakin120@gmail.com>
2025-01-08 17:35:32 +0100

    Add projects section

---
+++ public/index.js
@@ -0,0 +1,17 @@
+const gogolive = "/gogolive.png";
+const hotelbooking = "/hotelbooking.png";
+const htmlCssPortfolio = "/htmlcssportfolio.png";
+const myPortfolio = "/myportfolio.png";
+const realCastle = "/realCastle.png";
+const mankind4devBlog = "/mankind4devblog.png";
+const mankind4dev = "/mankind4dev.png";
+
+export {
+  gogolive,
+  hotelbooking,
+  htmlCssPortfolio,
+  myPortfolio,
+  realCastle,
+  mankind4devBlog,
+  mankind4dev
+};
