6d418d7cca95c2178d956ab29fee22fc2f312616
mankind4dev <ogunmakin120@gmail.com>
2025-01-12 07:32:10 +0100

    Fix get users

--- src/app/api/post/get/route.js
+++ src/app/api/post/get/route.js
@@ -1,41 +1,49 @@
-import User from '../../../../lib/models/user.model';
-import { connect } from '../../../../lib/mongodb/mongoose';
-import { currentUser } from '@clerk/nextjs/server';
+import Post from "@/lib/models/post.model";
+import { connect } from "@/lib/mongodb/mongoose";
 
 export const POST = async (req) => {
-  const user = await currentUser();
-
+  await connect();
+  const data = await req.json();
   try {
-    await connect();
-    const data = await req.json();
-
-    if (!user.publicMetadata.isAdmin) {
-      return new Response('Unauthorized', { status: 401 });
-    }
-
     const startIndex = parseInt(data.startIndex) || 0;
     const limit = parseInt(data.limit) || 9;
-    const sortDirection = data.sort === 'asc' ? 1 : -1;
-    const users = await User.find()
-      .sort({ createdAt: sortDirection })
+    const sortDirection = data.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
+      ...(data.userId && { userId: data.userId }),
+      ...(data.category &&
+        data.category !== 'null' &&
+        data.category !== 'undefined' && { category: data.category }),
+      ...(data.slug && { slug: data.slug }),
+      ...(data.postId && { _id: data.postId }),
+      ...(data.searchTerm && {
+        $or: [
+          { title: { $regex: data.searchTerm, $options: 'i' } },
+          { content: { $regex: data.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
 
-    const totalUsers = await User.countDocuments();
+    const totalPosts = await Post.countDocuments();
+
     const now = new Date();
+
     const oneMonthAgo = new Date(
       now.getFullYear(),
       now.getMonth() - 1,
       now.getDate()
     );
-    const lastMonthUsers = await User.countDocuments({
+
+    const lastMonthPosts = await Post.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
-    return new Response(JSON.stringify({ users, totalUsers, lastMonthUsers }), {
+
+    return new Response(JSON.stringify({ posts, totalPosts, lastMonthPosts }), {
       status: 200,
     });
   } catch (error) {
-    console.log('Error getting the users :', error);
-    return new Response('Error getting the users', { status: 500 });
+    console.log('Error getting posts:', error);
   }
 };
--- src/app/components/DashUsers.jsx
+++ src/app/components/DashUsers.jsx
@@ -41,7 +41,7 @@ export default function DashUsers() {
     );
   }
   return (
-    <div className='text-center table-auto overflow-x-scroll w-[100%] mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {user?.publicMetadata?.isAdmin && users.length > 0 ? (
         <>
           <Table hoverable className='shadow-md'>
